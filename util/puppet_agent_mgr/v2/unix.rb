8ddc4f7a9dace216a6e36e52d17a16d37cd09682
Richard Clamp <richardc@unixbeard.net>
2013-12-13 17:40:42 +0000

    v2/unix and v3/unix are nearly identical

--- util/puppet_agent_mgr/v2/unix.rb
+++ util/puppet_agent_mgr/v2/unix.rb
@@ -1,49 +1,14 @@
+require '%s/common/unix.rb' % File.dirname(File.dirname(__FILE__))
+
 module MCollective
   module Util
     module PuppetAgentMgr::V2
       module Unix
+        include ::MCollective::Util::PuppetAgentMgr::Common::Unix
         extend Unix
-        # is the agent daemon currently in the unix process list?
-        def daemon_present?
-          if File.exist?(Puppet[:pidfile])
-            return has_process_for_pid?(File.read(Puppet[:pidfile]))
-          end
 
-          return false
-        end
-
-        # is the agent currently applying a catalog
-        def applying?
-          return false if disabled?
-
-          return false unless File.exist?(Puppet[:puppetdlockfile])
-
-          if File::Stat.new(Puppet[:puppetdlockfile]).size > 0
-            return has_process_for_pid?(File.read(Puppet[:puppetdlockfile]))
-          end
-
-          return false
-        rescue => e
-          Log.warn("Could not determine if Puppet is applying a catalog: %s: %s: %s" % [e.backtrace.first, e.class, e.to_s])
-          return false
-        end
-
-        def signal_running_daemon
-          pid = File.read(Puppet[:pidfile])
-
-          if has_process_for_pid?(pid)
-            begin
-              Process.kill("USR1", Integer(pid))
-            rescue Exception => e
-              raise "Failed to signal the puppet agent at pid %s: %s" % [pid, e.to_s]
-            end
-          else
-            run_in_background
-          end
-        end
-
-        def has_process_for_pid?(pid)
-          !!Process.kill(0, Integer(pid)) rescue false
+        def agent_catalog_run_lockfile
+          Puppet[:puppetdlockfile]
         end
       end
     end
